e5df1b8d2a6db2971d26390e8b17ed821f0c5997
sathvika56 <skinspire2105@gmail.com>
2025-07-03 15:48:33 +0530

    Initial commit with Python script

---
+++ script/generate_output.py
@@ -0,0 +1,8 @@
+# script/generate_output.py
+
+def main():
+    with open("output.txt", "w") as f:
+        f.write("Hello, Sathvika!\nThis output is stored in JFrog.\n")
+
+if __name__ == "__main__":
+    main()
